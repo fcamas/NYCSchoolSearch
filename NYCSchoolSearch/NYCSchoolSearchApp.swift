38050c816ce6fc228309be14b9d621ac1ecdd3a1
fcamas <fcamas62@gmail.com>
2022-05-18 22:25:48 -0400

    Initial commit

---
+++ NYCSchoolSearch/NYCSchoolSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolSearchApp.swift
+//  NYCSchoolSearch
+//
+//  Created by Life on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
