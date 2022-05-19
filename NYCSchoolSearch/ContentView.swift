5ea3e7fe47e746f6c6268b8d07e3afed6feb523c
fcamas <fcamas62@gmail.com>
2022-05-18 22:54:49 -0400

    added school view

--- NYCSchoolSearch/ContentView.swift
+++ NYCSchoolSearch/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    var schoolNetwork = SchoolNetwork()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]){
+                    ForEach(schoolNetwork){name in
+                        SchoolCardView(name: name)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SchoolCardView: View {
+    
+    var name:String
+    var body: some View {
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 25)
+            shape.strokeBorder(lineWidth: 3)
+            Text(name)
+        }
     }
 }
